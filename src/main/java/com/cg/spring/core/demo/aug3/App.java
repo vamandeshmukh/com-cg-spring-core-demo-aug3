cbcac9264cf32da5fdc324374c29588452ae81c4
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-03 14:20:14 +0530

    Update App.java

--- src/main/java/com/cg/spring/core/demo/aug3/App.java
+++ src/main/java/com/cg/spring/core/demo/aug3/App.java
@@ -22,8 +22,24 @@ public class App {
 //		Employee employee = hey Spring container, get me the object;
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+//		Employee obj = context.getBean("employee", Employee.class);
 
-		Employee obj = context.getBean("employee", Employee.class);
+		Employee obj = context.getBean(Employee.class);
+		Employee obj2 = context.getBean(Employee.class);
+
+		System.out.println(obj.hashCode());
+		System.out.println(obj2.hashCode());
+		
+
+		System.out.println(obj.equals(obj2));
+
+		
+		Employee e1 = new Employee();
+		Employee e2 = new Employee();
+		
+		System.out.println(e1.hashCode());
+		System.out.println(e2.hashCode());
+				
 		obj.work();
 
 		System.out.println("End");
