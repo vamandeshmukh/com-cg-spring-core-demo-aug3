29acbe270628603c8725b89be526a5d5494023d6
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-03 17:26:51 +0530

    Commit
    Commit

--- src/main/java/com/cg/spring/core/demo/aug3/App2.java
+++ src/main/java/com/cg/spring/core/demo/aug3/App2.java
@@ -1,10 +1,23 @@
 package com.cg.spring.core.demo.aug3;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
 public class App2 {
-	
+
 	public static void main(String[] args) {
-		
+
+		System.out.println("Start");
+
+		ApplicationContext context = 
+		new AnnotationConfigApplicationContext(App2.class);
+		Customer customer = context.getBean(Customer.class);
+		customer.buy();
+
+		System.out.println("End");
+
 	}
-	
 
 }
---
+++ src/main/java/com/cg/spring/core/demo/aug3/Customer.java
@@ -0,0 +1,12 @@
+package com.cg.spring.core.demo.aug3;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+
+	public void buy() {
+		System.out.println("Customer buys products...");
+	}
+
+}
